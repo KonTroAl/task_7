1d50ed827b62da1c9bf337cd4fd69f87ab5ed419
KonTroAl <troshenkin.k@yandex.ru>
2020-11-12 21:14:55 +0500

    started task 3

--- task_3.py
+++ task_3.py
@@ -13,3 +13,34 @@
 arr[m]
 from statistics import median
 """
+import timeit
+import random
+from statistics import median
+
+origin_lst = [3, 4, 3, 3, 5, 3, 3]
+
+# m = int(input("Введи размер массива: "))
+
+# origin_lst = [random.randint(0, 100) for _ in range(2*m + 1)]
+
+right = []
+left = []
+
+
+def my_func(lst):
+    n = 0
+    my_median = origin_lst[n]
+    while True:
+        if
+        for i in range(len(origin_lst)-1):
+            if my_median < origin_lst[i + 1]:
+                left.append(origin_lst[i + 1])
+            elif my_median >= origin_lst[i + 1]:
+                right.append(origin_lst[i + 1])
+
+        return left, right
+
+
+print(origin_lst)
+print(median(origin_lst))
+print(my_func(origin_lst))
