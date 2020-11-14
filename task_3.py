e285dbed6c86a6a13aa171047455cd98ac21737c
KonTroAl <troshenkin.k@yandex.ru>
2020-11-14 13:05:59 +0500

    improve of task 3

--- task_3.py
+++ task_3.py
@@ -17,15 +17,15 @@ import timeit
 import random
 from statistics import median
 
-# origin_lst = [3, 4, 3, 3, 5, 3, 3]
 
-m = int(input("Введи размер массива: "))
+def my_func():
+    m = int(input("Количество элементов массива будет получено согласно формуле 2m + 1. Введите число m: "))
+    len_list = 2 * m + 1
+    origin_lst = []
+    while len(origin_lst) < len_list:
+        origin_set = {random.randint(0, 100) for _ in range(len_list)}
+        origin_lst = list(origin_set)
 
-origin_set = {random.randint(0, 100) for _ in range(2 * m + 1)}
-origin_lst = list(origin_set)
-
-
-def my_func(lst):
     right = []
     left = []
     n = 0
@@ -37,14 +37,41 @@ def my_func(lst):
             elif my_median > origin_lst[i]:
                 right.append(origin_lst[i])
         if len(right) == len(left):
-            return origin_lst[n]
+            return f"Медианой списка: {origin_lst} явялется {my_median} / {median(origin_lst)}"
         n += 1
         right.clear()
         left.clear()
 
 
-print(origin_lst)
-print(median(origin_lst))
-print(my_func(origin_lst))
+print(my_func())
 
 """Медианой будет тот элемент при котором получится разделить исходный список на два одинаковых по размеру массива"""
+
+# m = int(input("Введи размер массива: "))
+#
+# origin_set = {random.randint(0, 100) for _ in range(2 * m + 1)}
+# origin_lst = list(origin_set)
+
+# def my_func(lst):
+#     right = []
+#     left = []
+#     n = 0
+#     while len(right) == 0 and len(left) == 0:
+#         my_median = origin_lst[n]
+#         for i in range(len(origin_lst)):
+#             if my_median < origin_lst[i]:
+#                 left.append(origin_lst[i])
+#             elif my_median > origin_lst[i]:
+#                 right.append(origin_lst[i])
+#         if len(right) == len(left):
+#             return origin_lst[n]
+#         n += 1
+#         right.clear()
+#         left.clear()
+#
+#
+# print(origin_lst)
+# print(median(origin_lst))
+# print(my_func(origin_lst))
+#
+# """Медианой будет тот элемент при котором получится разделить исходный список на два одинаковых по размеру массива"""
